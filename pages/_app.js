9f8136b288ac240278b33fb0075a3b80f0aadad2
unknown <felipescherer25@gmail.com>
2021-01-28 20:52:44 -0300

    Fim da segunda aula-Linter-Rotas-States

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 import db from '../db.json';
 
@@ -25,23 +26,25 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet" />  
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet" />
         <link rel="icon" href="./images/cat-icon.png" />
         <title>Animal Quiz | Imersão Alura</title>
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import QuizLogo from '../src/components/QuizLogo';
+import Input from '../src/components/Input';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -12,8 +16,6 @@ import QuizLogo from '../src/components/QuizLogo'
 //   background-size: cover;
 //   background-position: center;
 // `;
-
-
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -27,29 +29,50 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-      <QuizLogo />
-      <Widget>
-        <Widget.Header>
-          <h1>{db.title}</h1>
-        </Widget.Header>
-        <Widget.Content>
-          <p>{db.description}</p>
-        </Widget.Content>
-      </Widget>
-      <Widget>
-        <Widget.Header>
-          <h1>Quizes da galera</h1>
-        </Widget.Header>
-        <Widget.Content>
-          <p>Ex eu velit excepteur cillum.Esse incididunt ipsum incididunt fugiat dolore elit.</p>
-        </Widget.Content>
-      </Widget>
-      <Footer />
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+            <form onSubmit={(eventInfo) => {
+              eventInfo.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              // router que manda para a prox pagina
+            }}
+            >
+              <Input
+                placeholder="Escreva seu nome"
+                onChange={(eventInfo) => {
+                  // name = eventInfo.target.value;
+                  setName(eventInfo.target.value);
+                }}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Ex eu velit excepteur cillum.Esse incididunt ipsum incididunt fugiat dolore elit.</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/ofelipescherer/alura-quiz"/>
+      <GitHubCorner projectUrl="https://github.com/ofelipescherer/alura-quiz" />
     </QuizBackground>
   );
 }
